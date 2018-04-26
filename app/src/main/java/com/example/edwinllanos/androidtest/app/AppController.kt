524d5e85dcb2f9a9e3c2ae56fb5ab999ca5f574b
Edwin Llanos <ellanos@merqueo.com>
2018-04-26 12:11:17 -0500

    Install dependecy for dagger

--- app/src/main/java/com/example/edwinllanos/androidtest/app/AppController.kt
+++
@@ -1,6 +0,0 @@
-package com.example.edwinllanos.androidtest.app
-
-import android.app.Application
-
-class AppController : Application() {
-}
---
+++ app/src/main/java/com/example/edwinllanos/androidtest/di/Application.kt
@@ -0,0 +1,9 @@
+package com.example.edwinllanos.androidtest.di
+
+import android.app.Application
+
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
